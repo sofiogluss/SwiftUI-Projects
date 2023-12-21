c24ddfd6918efd95c52d09756c62845deec2e182
sofiogluss <154444832+sofiogluss@users.noreply.github.com>
2023-12-21 14:22:16 +0300

    User Interface and User Experience App Design
    1 - Complex Card Layout
    2 - Gradient Animation
    3 - Resuable Components
    4 - Light and Dark Mode

---
+++ Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Şafak Selim Sofioğlu on 8.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hike/Hike/Screen/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Şafak Selim Sofioğlu on 8.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
